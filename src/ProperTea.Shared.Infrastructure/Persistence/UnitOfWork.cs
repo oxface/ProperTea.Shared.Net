16d5bc835f4cbbfee069e79d3f5a51c914884d37
Oxface <r.g.klapchuk@gmail.com>
2025-09-16 19:01:40 +0000

    Fixed broken references.

--- src/ProperTea.Shared.Infrastructure/Persistence/UnitOfWork.cs
+++ src/ProperTea.Shared.Infrastructure/Persistence/UnitOfWork.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
-using ProperTea.Application.Shared;
 using ProperTea.Domain.Shared;
 using ProperTea.Domain.Shared.Events;
+using ProperTea.Shared.Application;
 
 namespace ProperTea.Infrastructure.Shared.Persistence;
 
